d95b4ef4cc3de265033276abd8174b5f85fbe526
Devraj <devrajshivam02@gmail.com>
2026-08-07 21:10:31 +0530

    chat bot fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import VoiceAssistant from "./components/VoiceAssistant.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { SocketProvider } from "./context/SocketContext.jsx";
 import "./index.css";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <AuthProvider>
         <SocketProvider>
           <App />
+          <VoiceAssistant />
         </SocketProvider>
       </AuthProvider>
     </BrowserRouter>
